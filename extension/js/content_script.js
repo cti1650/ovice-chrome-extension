183c3a417dbb504a718e60a801cfdca1abcc0bc4
cti1650 <cti1650@gmail.com>
2021-03-26 07:26:14 +0900

    add badgeBtn

--- extension/js/content_script.js
+++ extension/js/content_script.js
@@ -9,6 +9,23 @@ function testBtn(){
   });
   alert(userList.join('\\n'));
 };
+function badgeBtn(){
+  let arr = [];
+  ovice.webrtc.users().forEach((user) => {
+    if (user.name && user.badge) {
+      if (arr[user.badge]) {
+        arr[user.badge]['count'] += 1, arr[user.badge]['users'].push(user.name)
+      } else {
+        arr.push(user.badge);
+        arr[user.badge] = {
+          'count': 1,
+          'users': [user.name]
+        }
+      }
+    }
+  });
+  alert(arr.toString());
+}
 function megaphone(){
   ovice.setMegaphone(!ovice.megaphone());
 };
@@ -80,7 +97,8 @@ btns.style.flexDirection = "column";
 btns.innerHTML = `
 <button onClick="kanyoKanjiCyusen();" style="font-size:1.4rem;width:50px;height:50px;" title="Kanyo幹事抽選補助ツール"><i class="fa fa-child"></i></button>
 <button onClick="kanyoCyusen();" style="font-size:1.4rem;width:50px;height:50px;" title="Kanyo抽選補助ツール"><i class="fa fa-cube"></i></button>
-<button onClick="testBtn();" style="font-size:1.4rem;width:50px;height:50px;" title="ユーザーリスト"><i class="fa fa-users"></i></i></button>
+<button onClick="testBtn();" style="font-size:1.4rem;width:50px;height:50px;" title="ユーザーリスト"><i class="fa fa-users"></i></button>
+<button onClick="badgeBtn();" style="font-size:1.4rem;width:50px;height:50px;" title="アイコンリスト"><i class="fa fa-bullseye"></i></button>
 <button onClick="megaphone();" style="font-size:1.4rem;width:50px;height:50px;" title="メガホン"><i class="fa fa-bullhorn"></i></button>
 <button onClick="archives();" style="font-size:1.4rem;width:50px;height:50px;" title="アーカイブ"><i class="fa fa-archive"></i></button>
 <button onClick="IT_daigaku();" style="font-size:1.4rem;width:50px;height:50px;" title="しまぶーのIT大学"><i class="fa fa-graduation-cap"></i></button>
